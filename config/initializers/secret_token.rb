fbf05fcca51722bc26b1ffc0295b61e88be46a3d
Nerian <siotopo@gmail.com>
2010-11-21 18:29:50 +0100

    first commit as rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goofb::Application.config.secret_token = '1f6ac5dee7eb597cb7f98c43f1a5116a776269a1d8322bf3143b3dd512e737712b6719e806c60dbdf7b6e5b44556f6980951bf5a46f31e57cb6614c7a0bb5e90'
