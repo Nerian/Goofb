9010856e49d543f4919efb44a0a19c5d5bd58c16
Nerian <siotopo@gmail.com>
2011-01-17 21:28:31 +0100

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def facebook_client
+    client = FacebookOAuth::Client.new(
+        :application_id => '104108729662859',
+        :application_secret => 'acb7a93d8e5938f3b5663c81b0862c88',
+        :callback => 'http://young-lightning-861.heroku.com/session/create'
+    )    
+  end
 end
