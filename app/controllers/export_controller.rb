1e02a10b88107daeb9e72faabef63fee9c8103ac
Nerian <siotopo@gmail.com>
2011-02-17 16:40:15 +0100

    example

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -5,6 +5,7 @@ class ExportController < ApplicationController
   
   def create
     #send_data generate_tgz("#{RAILS_ROOT}/tmp/myfile_#{Process.pid}"), :filename => 'Facebook-export.tgz'
+    File.open("#{RAILS_ROOT}/tmp/example.txt", 'w') {|f| f.write("Example") }      
     send_file "#{RAILS_ROOT}/tmp/example.txt", :filename => 'Facebook-export.txt'    
   end
 end
