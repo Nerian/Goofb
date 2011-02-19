1abeefea2314729e87389874ede016dd40f20f39
Nerian <siotopo@gmail.com>
2011-02-19 15:49:44 +0100

    change application secret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   
   def facebook_client
     client = FacebookOAuth::Client.new(
-        :application_id => '104108729662859',
-        :application_secret => 'acb7a93d8e5938f3b5663c81b0862c88',
+        :application_id => ENV['FACEBOOK_APPLICATION_ID'],
+        :application_secret => ENV['FACEBOOK_APPLICATION_SECRET'],
         :callback => 'http://young-lightning-861.heroku.com/session/create'
     )    
   end
