9c242792c7b6d06d697d2c2af5c33f1a783e9337
Nerian <siotopo@gmail.com>
2011-02-19 13:35:20 +0100

    conditionals

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -7,10 +7,10 @@ class ExportController < ApplicationController
     client = get_client 
     @user = client.me.info                       
     
-    generate_profile_file(JSON.pretty_generate(@user))    
-    generate_album_file
-    generate_wall_file
-    generate_friend_list_file
+    generate_profile_file(JSON.pretty_generate(@user)) if params[:profile]    
+    generate_album_file if params[:album]
+    generate_wall_file if params[:wall]
+    generate_friend_list_file if params[:friends]
     
     send_data generate_tgz("#{RAILS_ROOT}/tmp/export#{Process.pid}"), :filename => 'Facebook-export.tgz'    
   end
