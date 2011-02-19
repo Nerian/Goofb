b020701042cb93f167cba12bca93fb8c9226ad4d
Nerian <siotopo@gmail.com>
2011-02-19 13:46:16 +0100

    show notice if no options was selected

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -7,10 +7,15 @@ class ExportController < ApplicationController
     client = get_client 
     @user = client.me.info                       
     
-    generate_profile_file(JSON.pretty_generate(@user)) if params[:profile]    
-    generate_album_file if params[:album]
-    generate_wall_file if params[:wall]
-    generate_friend_list_file if params[:friends]
+    if params[:profile] or params[:album] or params[:wall] or params[:friends] do    
+      generate_profile_file(JSON.pretty_generate(@user)) if params[:profile]    
+      generate_album_file if params[:album]
+      generate_wall_file if params[:wall]
+      generate_friend_list_file if params[:friends]                              
+    else
+      render :new, :notice => "You need to select at least one option" 
+    end
+    
     
     send_data generate_tgz("#{RAILS_ROOT}/tmp/export#{Process.pid}"), :filename => 'Facebook-export.tgz'    
   end
