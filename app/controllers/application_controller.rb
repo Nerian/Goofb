65eecc1eb0b9b9a74752dde5f9d44bb0528b0ac6
Nerian <siotopo@gmail.com>
2011-04-19 19:16:19 +0200

    jujuj

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,8 +30,8 @@ class ApplicationController < ActionController::Base
       Dir.mkdir("#{RAILS_ROOT}/tmp/export/album/#{album_name}") unless File.exists?("#{RAILS_ROOT}/tmp/export/album/#{album_name}")
       photos = graph.get_connections(album['id'], 'photos')
       photos.each do |photo|
-        photo_name = photo['name'].downcase.tr(' ', '_')
-        File.open("#{RAILS_ROOT}/tmp/export/album/#{album_name}/#{photo_name}", 'w'){ |f| f.write(photo['source'])}
+        #photo_name = photo['name'].downcase.tr(' ', '_')
+        #File.open("#{RAILS_ROOT}/tmp/export/album/#{album_name}/#{photo_name}", 'w'){ |f| f.write(photo['source'])}
       end
     end                     
   end         
