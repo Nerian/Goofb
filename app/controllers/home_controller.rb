afc7b2f97521cda438d312ac5f18ae492eee7283
Nerian <siotopo@gmail.com>
2011-04-19 16:51:13 +0200

    oauth with koala

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,4 @@
 class HomeController < ApplicationController
-  def index
-  end
-  
-  def show
-    client = facebook_client                 
-    access_token = client.authorize(:code => session["token"]) 
-    @user = client.me.info       
-    render :json => JSON.pretty_generate(@user)   
-  end
+  def index    
+  end 
 end
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,14 +1,16 @@
 class SessionController < ApplicationController
   def new      
-    client = facebook_client                                                      
-    redirect_to client.authorize_url(:scope=> 'user_photos,user_about_me,read_stream')
+    @oauth = Koala::Facebook::OAuth.new(ENV['FACEBOOK_APPLICATION_ID'], ENV['FACEBOOK_APPLICATION_SECRET'], 'http://young-lightning-861.heroku.com/session/create')
+    puts @oauth
+    
+    redirect_to @oauth.url_for_oauth_code(:permissions => 'user_photos,user_about_me,read_stream')   
   end
   
   def create
-    client = facebook_client                 
-    access_token = client.authorize(:code => params["code"]) 
-    session["token"] = params["code"]    
-    @user = client.me.info.to_yaml
+    @oauth = Koala::Facebook::OAuth.new(ENV['FACEBOOK_APPLICATION_ID'], ENV['FACEBOOK_APPLICATION_SECRET'], 'http://young-lightning-861.heroku.com/session/create')
+    
+    access_token = @oauth.get_access_token(params["code"]) 
+    session["token"] = params["code"]
     redirect_to export_new_path
   end      
 end
