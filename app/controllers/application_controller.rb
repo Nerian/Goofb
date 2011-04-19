0d7936bdf4bbc7cc426bcfa9686c06b4fec5d6f9
Nerian <siotopo@gmail.com>
2011-04-19 20:43:00 +0200

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,9 +16,7 @@ class ApplicationController < ActionController::Base
     Dir.mkdir("#{RAILS_ROOT}/tmp/export") unless File.exists?("#{RAILS_ROOT}/tmp/export")
     File.open("#{RAILS_ROOT}/tmp/export/profile.txt", 'w') {|f| f.write(JSON.pretty_generate(graph.get_object('me'))) }        
   end         
-  
-  
-  
+      
   def generate_album_file(graph)
     Dir.mkdir("#{RAILS_ROOT}/tmp/export") unless File.exists?("#{RAILS_ROOT}/tmp/export")
     Dir.mkdir("#{RAILS_ROOT}/tmp/export/albums") unless File.exists?("#{RAILS_ROOT}/tmp/export/albums")
@@ -35,46 +33,11 @@ class ApplicationController < ActionController::Base
         else          
           photo_name = photo['name'].downcase.tr(' ', '_') 
         end
-        url = URI.parse(photo['source'])   
-        
-        require 'open-uri'
-        File.open("#{RAILS_ROOT}/tmp/export/albums/#{album_name}/#{photo_name}", 'wb') do |dest|
-          open(URI.parse(photo['source'], 'rb') do |src|
-            dest.write(src.read)
-          end
-        end
-        
-        #File.open("#{RAILS_ROOT}/tmp/export/albums/#{album_name}/#{photo_name}.jpg", 'w'){ |f| f.write(Net::HTTP.start(url.host, url.port) {|http| http.get(url.path)}.body)}
+        url = URI.parse(photo['source'])           
+        File.open("#{RAILS_ROOT}/tmp/export/albums/#{album_name}/#{photo_name}.jpg", 'w'){ |f| f.write(Net::HTTP.start(url.host, url.port) {|http| http.get(url.path)}.body)}
       end
     end                     
-  end         
-    
-  #def generate_album_file(client)
-  #  Dir.mkdir("#{RAILS_ROOT}/tmp/export") unless File.exists?("#{RAILS_ROOT}/tmp/export")
-  #  Dir.mkdir("#{RAILS_ROOT}/tmp/export/album") unless File.exists?("#{RAILS_ROOT}/tmp/export/album")
-  #  
-  #  client.me.albums['data'].each do |album|   
-  #    
-  #    puts 'ALBUM'
-  #    puts JSON.pretty_generate(album) 
-  #    puts ' '                     
-  #    puts " --> client.me.albums(album['id'])"
-  #    puts JSON.pretty_generate(client.me.albums(album['id']))
-  #    puts ' '
-  #    
-  #    client.me.albums(album['id']).each do |photo|
-  #      puts ' A photo, in the each '
-  #      puts JSON.pretty_generate(photo)
-  #      puts ' '
-  #      source = client.me.photos(photo['id']).source # not sure if you need to do source['data']
-  #      file = open(source)
-  #      content = file.read()
-  #      File.open("#{RAILS_ROOT}/tmp/export/album/#{photo['id']}.jpg", 'w') {|f| content }
-  #    end
-  #  end
-            
-  #  File.open("#{RAILS_ROOT}/tmp/export/album/album.txt", 'w') {|f| f.write(JSON.pretty_generate(client.me.albums)) }    
-  #end                   
+  end                    
   
   def generate_wall_file(client)
     Dir.mkdir("#{RAILS_ROOT}/tmp/export") unless File.exists?("#{RAILS_ROOT}/tmp/export")
