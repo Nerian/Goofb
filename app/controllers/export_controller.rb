2fe610a37443a5a57dd38bdb16f86e5567dae3c3
Nerian <siotopo@gmail.com>
2011-04-19 18:41:17 +0200

    fix

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -7,9 +7,9 @@ class ExportController < ApplicationController
     graph = get_graph                    
     if params[:profile] or params[:album] or params[:wall] or params[:friends] then    
       generate_profile_file(graph) if params[:profile]    
-      #generate_album_file(graph) if params[:album]
-      #generate_wall_file(graph) if params[:wall]
-      #generate_friend_list_file(graph) if params[:friends]                              
+      generate_album_file(graph) if params[:album]
+      generate_wall_file(graph) if params[:wall]
+      generate_friend_list_file(graph) if params[:friends]                              
       send_data generate_tgz, :filename => 'Facebook-export.tgz'    
     else              
       redirect_to export_new_path, :notice => "You need to provide at least one option"
