86d37a5973add3bce67d8015b36fcf0b34b02a56
Nerian <siotopo@gmail.com>
2011-04-25 15:29:34 +0200

    js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,27 +1,31 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-$("input[name='album']").change(function()
-{
-    if( $(this).is(':checked') )
-    {
-        $("input[type='checkbox']","ul").attr('checked',true);
-    } else
-    {                                           ,
-        $("input[type='checkbox']","ul").attr('checked',false);
-    }
-});
 
-$("input[type='checkbox']","ul").each(function()
-{
-    $(this).change(function()
-    {
-        if( ! $(this).is(':checked') )
-        {
-            $("input[name='album']").attr('checked',false);
-        }
-        if( $("input[type='checkbox']","ul").length == $("input[type='checkbox']:checked","ul").length )
-        {
-            $("input[name='album']").attr('checked',true);
-        }
-    });
+
+$(document).ready(function(){
+	$("input[name='album']").change(function()
+	{
+	    if( $(this).is(':checked') )
+	    {
+	        $("input[type='checkbox']","ul").attr('checked',true);
+	    } else
+	    {                                           ,
+	        $("input[type='checkbox']","ul").attr('checked',false);
+	    }
+	});
+
+	$("input[type='checkbox']","ul").each(function()
+	{
+	    $(this).change(function()
+	    {
+	        if( ! $(this).is(':checked') )
+	        {
+	            $("input[name='album']").attr('checked',false);
+	        }
+	        if( $("input[type='checkbox']","ul").length == $("input[type='checkbox']:checked","ul").length )
+	        {
+	            $("input[name='album']").attr('checked',true);
+	        }
+	    });
+	});          
 });
