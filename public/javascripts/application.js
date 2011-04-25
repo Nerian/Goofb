46f160bc3af2bde2c843d6774bb382151eb6f8bd
Nerian <siotopo@gmail.com>
2011-04-25 15:27:08 +0200

    js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,30 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+<script>
+$("input[name='album']").change(function()
+{
+    if( $(this).is(':checked') )
+    {
+        $("input[type='checkbox']","ul").attr('checked',true);
+    } else
+    {                                           ,
+        $("input[type='checkbox']","ul").attr('checked',false);
+    }
+});
+
+$("input[type='checkbox']","ul").each(function()
+{
+    $(this).change(function()
+    {
+        if( ! $(this).is(':checked') )
+        {
+            $("input[name='album']").attr('checked',false);
+        }
+        if( $("input[type='checkbox']","ul").length == $("input[type='checkbox']:checked","ul").length )
+        {
+            $("input[name='album']").attr('checked',true);
+        }
+    });
+});
+</script>
