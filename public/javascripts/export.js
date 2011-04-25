08a663126f0e0d3da20a99516ab2ce24f3c37d7c
Nerian <siotopo@gmail.com>
2011-04-25 15:24:15 +0200

    js

---
+++ public/javascripts/export.js
@@ -0,0 +1,25 @@
+$("input[name='album']").change(function()
+{
+    if( $(this).is(':checked') )
+    {
+        $("input[type='checkbox']","ul").attr('checked',true);
+    } else
+    {
+        $("input[type='checkbox']","ul").attr('checked',false);
+    }
+});
+
+$("input[type='checkbox']","ul").each(function()
+{
+    $(this).change(function()
+    {
+        if( ! $(this).is(':checked') )
+        {
+            $("input[name='album']").attr('checked',false);
+        }
+        if( $("input[type='checkbox']","ul").length == $("input[type='checkbox']:checked","ul").length )
+        {
+            $("input[name='album']").attr('checked',true);
+        }
+    });
+});
