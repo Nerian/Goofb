439a1ab3fff73a21b8fe1efbf15874789023e451
Nerian <siotopo@gmail.com>
2011-04-27 16:29:33 +0200

    albums

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -13,9 +13,9 @@ class ExportController < ApplicationController
   def create    
     graph = get_graph                     
     puts params
-    if params[:profile] or params[:album] or params[:wall] or params[:friends] or params[:albums] then    
+    if params[:profile] or params[:wall] or params[:friends] or params[:albums] then    
       generate_profile_file(graph) if params[:profile]    
-      generate_album(graph, params[:albums]) if params[:album]
+      generate_album(graph, params[:albums]) if params[:albums]
       generate_wall_file(graph) if params[:wall]
       generate_friend_list_file(graph) if params[:friends]                              
       send_data generate_tgz, :filename => 'Facebook-export.tgz'    
